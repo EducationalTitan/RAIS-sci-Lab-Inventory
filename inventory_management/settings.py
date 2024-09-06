c8b9f43475e57e511a9dffa6acb52ea3bff1e547
EducationalTitan <111095726+EducationalTitan@users.noreply.github.com>
2024-09-06 09:09:12 +0100

    Update settings.py

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -25,9 +25,8 @@ SECRET_KEY = 'django-insecure-y3ehil7zqx%c2b5rw&3ot6$kc5x%lsm-yb1g12cav7#57ah+jn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-if 'RENDER' in os.environ:
-ALLOWED_HOSTS = ['https://rais-sci-lab-inventory.onrender.com', 'localhost', '127.0.0.1']
-
+if some_condition:
+    ALLOWED_HOSTS = ['https://rais-sci-lab-inventory.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
